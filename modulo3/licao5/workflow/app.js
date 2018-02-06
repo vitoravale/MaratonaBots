92cad3bc46b41fc09626e0113071514129357bd3
Vitor A. Vale <vitoravale@outlook.com>
2018-02-05 23:46:58 -0200

    fix typo

--- modulo3/licao5/workflow/app.js
+++ modulo3/licao5/workflow/app.js
@@ -19,7 +19,7 @@ server.listen(port, () => {
 
 const connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORd
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 })
 
 const bot = new builder.UniversalBot(connector)
